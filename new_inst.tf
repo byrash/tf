067695deb390b06078f07c628c844230c9b685f1
Shivaji Byrapaneni <Shivaji.Byrapaneni@pexa.com.au>
2020-12-16 13:48:32 +1100

    First EC2 Instance

---
+++ new_inst.tf
@@ -0,0 +1,11 @@
+provider "aws" {
+  region = "ap-southeast-2"
+}
+
+resource "aws_instance" "myFristEc2UsingTF" {
+  ami           = "ami-09f765d333a8ebb4b"
+  instance_type = "t2.micro"
+  tags = {
+    "Name" = "myFristEc2UsingTF"
+  }
+}
