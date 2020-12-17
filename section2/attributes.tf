38b3dd06dfe61903d92d468a6da6bc965d9ae1e7
Shivaji Byrapaneni <Shivaji.Byrapaneni@pexa.com.au>
2020-12-17 13:33:17 +1100

    Section2 references

--- section2/attributes.tf
+++ section2/attributes.tf
@@ -1,20 +1,20 @@
-provider "aws" {
-  region  = "ap-southeast-2"
-  version = "~> 3.0"
-}
+# provider "aws" {
+#   region  = "ap-southeast-2"
+#   version = "~> 3.0"
+# }
 
-resource "aws_eip" "eip" {
-  vpc = true
-}
+# resource "aws_eip" "eip" {
+#   vpc = true
+# }
 
-output "eipOut" {
-  value = aws_eip.eip.public_ip
-}
+# output "eipOut" {
+#   value = aws_eip.eip.public_ip
+# }
 
-resource "aws_s3_bucket" "s3BucketUsingTF" {
-  bucket = "byrash-s3-bucket-using-tf"
-}
+# resource "aws_s3_bucket" "s3BucketUsingTF" {
+#   bucket = "byrash-s3-bucket-using-tf"
+# }
 
-output "s3Bucketout" {
-  value = aws_s3_bucket.s3BucketUsingTF.bucket_domain_name
-}
+# output "s3Bucketout" {
+#   value = aws_s3_bucket.s3BucketUsingTF.bucket_domain_name
+# }
---
+++ section2/reference.tf
@@ -0,0 +1,21 @@
+provider "aws" {
+  region  = "ap-southeast-2"
+  version = "~> 3.0"
+}
+
+resource "aws_instance" "myFristEc2UsingTF" {
+  ami           = "ami-09f765d333a8ebb4b"
+  instance_type = "t2.micro"
+  tags = {
+    "Name" = "myFristEc2UsingTF"
+  }
+}
+
+resource "aws_eip" "eip" {
+  vpc = true
+}
+
+resource "aws_eip_association" "eip_assoc" {
+  instance_id   = aws_instance.myFristEc2UsingTF.id
+  allocation_id = aws_eip.eip.id
+}
