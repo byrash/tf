5d0fb28549d28609ec114341d2df2cab3459c504
Shivaji Byrapaneni <Shivaji.Byrapaneni@pexa.com.au>
2020-12-18 07:16:05 +1100

    Local variable override using tfvars

--- section2/reference.tf
+++ section2/reference.tf
@@ -5,17 +5,17 @@ provider "aws" {
 
 resource "aws_instance" "myFristEc2UsingTF" {
   ami           = "ami-09f765d333a8ebb4b"
-  instance_type = "t2.micro"
+  instance_type = var.instance_type
   tags = {
     "Name" = "myFristEc2UsingTF"
   }
 }
 
-resource "aws_eip" "eip" {
-  vpc = true
-}
+# resource "aws_eip" "eip" {
+#   vpc = true
+# }
 
-resource "aws_eip_association" "eip_assoc" {
-  instance_id   = aws_instance.myFristEc2UsingTF.id
-  allocation_id = aws_eip.eip.id
-}
+# resource "aws_eip_association" "eip_assoc" {
+#   instance_id   = aws_instance.myFristEc2UsingTF.id
+#   allocation_id = aws_eip.eip.id
+# }
